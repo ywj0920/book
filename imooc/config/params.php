dbcabb020b8e21d26a946fcefaaab7e6e5e82151
ywj0920 <931984582@qq.com>
2018-02-09 14:02:05 +0800

    first commit

---
+++ imooc/config/params.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'domain' => [
+        'www'=>'http://book.imooc.test',
+        'm'=>'http://book.imooc.test/m',
+        'web'=>'http://book.imooc.test/web',
+    ],
+    'upload' => [
+        'avater' => '/uploads/avater',
+        'brand' => '/uploads/brand',
+        'book' => '/uploads/book',
+
+    ]
+];
---
+++ imooc/modules/weixin/controllers/MsgController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\modules\weixin\controllers;
+
+use app\common\components\BaseWebController;
+
+class MsgController extends BaseWebController{
+
+    public function actionIndex(){
+        return "hello word";
+    }
+}
