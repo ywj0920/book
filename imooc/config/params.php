9ab21bb6ad16d163dc6957577f16c7a4e539ca1b
ywj0920 <931984582@qq.com>
2018-02-10 12:03:26 +0800

    first commit

--- imooc/config/params.php
+++ imooc/config/params.php
@@ -11,5 +11,11 @@ return [
         'brand' => '/uploads/brand',
         'book' => '/uploads/book',
 
-    ]
+    ],
+    'weixin' => [
+        'appid' =>'wx8f24038012e91a64',
+        'sk' => '',
+        'token' => 'ywj931984582',
+        'aeskey' => 'HLy3jDqT4Olg9anPY4wvF6msnaxSSc6jsp28pEdDOnk'
+    ],
 ];
